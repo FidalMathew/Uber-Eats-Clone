a580564357100e2a8e8331f0f5e98a783384ddd4
Fidal <fidal15perfect@gmail.com>
2022-07-12 21:12:00 +0530

    added searchbar and categories

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 
 const home = () => {
     return (
-        <SafeAreaView style={{ paddingTop: 40 }}>
-            <HeaderTabs />
+        <SafeAreaView style={{ paddingTop: 40, backgroundColor: "#eee", flex: 1 }}>
+            <View style={{ backgroundColor: "white", padding: 15 }}>
+                <HeaderTabs />
+                <SearchBar />
+            </View>
+            <Categories />
         </SafeAreaView>
     )
 }
