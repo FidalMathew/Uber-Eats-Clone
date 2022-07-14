4a99318f3769bc2d2b1048065207bd1781b7c48b
Fidal <fidal15perfect@gmail.com>
2022-07-14 13:51:22 +0530

    add bottom tabs

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,47 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-const SearchBar = () => {
+const SearchBar = ({ setCity }) => {
+
+    const [search, setSearch] = useState("")
+
     return (
-        <View style={{ marginTop: 15, flexDirection: "row" }}>
-            <GooglePlacesAutocomplete placeholder='Search'
-                styles={{
-                    textInput: {
-                        backgroundColor: "#eee",
-                        borderRadius: 20,
-                        fontWeight: "700",
-                        marginTop: 7
-                    },
-                    textInputContainer: {
-                        backgroundColor: "#eee",
-                        borderRadius: 50,
+        <View style={{
+            marginTop: 10,
+            flexDirection: "row",
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            alignItems: "center",
+            padding: 8
+        }}>
+            <View style={{ marginLeft: 10 }}>
+                <Ionicons name='location-sharp' size={24} />
+            </View>
+            <View style={{ flex: 1 }}>
+                <TextInput placeholder='Search' onChangeText={(text) => setSearch(text)} />
+            </View>
+            <TouchableOpacity onPress={() => setCity(search)}  >
+                <View
+                    style={{
                         flexDirection: "row",
-                        alignItems: "center",
-                        marginRight: 10
-                    }
-                }}
-
-                renderLeftButton={() => <View style={{ marginLeft: 10 }}>
-                    <Ionicons name='location-sharp' size={24} />
-                </View>}
-
-                renderRightButton={() =>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            marginRight: 8,
-                            backgroundColor: "white",
-                            padding: 9,
-                            borderRadius: 30,
-                            alignItems: "center"
-                        }}>
-                        <AntDesign name="clockcircle" size={11} style={{ marginRight: 6 }} />
-                        <Text>Search</Text>
-                    </View>}
-            />
-        </View>
+                        marginRight: 8,
+                        backgroundColor: "white",
+                        padding: 9,
+                        borderRadius: 30,
+                        alignItems: "center"
+                    }}>
+                    <AntDesign name="clockcircle" size={11} style={{ marginRight: 6 }} />
+                    <Text>Search</Text>
+                </View>
+            </TouchableOpacity>
+        </View >
     )
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,58 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, ScrollView, SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
+import RestaurantItems from '../components/RestaurantItems'
+import { Divider } from 'react-native-elements'
+import BottomTabs from '../components/BottomTabs'
 
 const home = () => {
+
+    const [restaurantData, setRestaurantData] = useState([])
+    const [city, setCity] = useState("")
+
+    const getRestaurants = () => {
+        setRestaurantData(
+            [
+                {
+                    image: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
+                    name: "Farmhouse Kitchen Thai Cuisine",
+                    rating: "4.5"
+                },
+                {
+                    image: "https://www.fabhotels.com/blog/wp-content/uploads/2018/10/1000x650-29.jpg",
+                    name: "Great India Palace",
+                    rating: "4.7"
+                },
+                {
+                    image: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
+                    name: "Khana Khazana",
+                    rating: "4.3"
+                }
+            ]
+        )
+    }
+
+    useEffect(() => {
+        getRestaurants();
+    }, [city])
+
+
     return (
-        <SafeAreaView style={{ paddingTop: 40, backgroundColor: "#eee", flex: 1 }}>
+        <View style={{ marginTop: 20, backgroundColor: "#eee", flex: 1 }}>
             <View style={{ backgroundColor: "white", padding: 15 }}>
                 <HeaderTabs />
-                <SearchBar />
+                <SearchBar setCity={setCity} />
             </View>
-            <Categories />
-        </SafeAreaView>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categories />
+                <RestaurantItems restaurantData={restaurantData} />
+            </ScrollView>
+
+            <Divider width={1} />
+            <BottomTabs />
+        </View>
     )
 }
 
